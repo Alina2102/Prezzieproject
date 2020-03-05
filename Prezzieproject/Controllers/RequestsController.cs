36def613eee8472cf8b5b804867e53231390f91d
Benedikt Strozynski <strozynskib@yahoo.de>
2020-03-05 16:18:24 +0200

    Request List modified

--- Prezzieproject/Controllers/RequestsController.cs
+++ Prezzieproject/Controllers/RequestsController.cs
@@ -14,7 +14,13 @@ namespace Prezzieproject.Controllers
         {
 
             var requestsList = new List<RequestsModel>{
-                new RequestsModel() { name = "Brezel", country = "Germany", description = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now }
+                new RequestsModel() { userName = "Brezel", countrySouv = "Germany" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Spätzle", countrySouv = "Germany" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Toast", countrySouv = "Germany" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Kuchen", countrySouv = "Italien" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Käse", countrySouv = "Frankreich" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Tomaten", countrySouv = "Italien" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now },
+                new RequestsModel() { userName = "Bananen", countrySouv = "Spanien" , descriptionSouv = "Ich möchte Aufbackbrezeln vom Aldi Süd!", requestDate = DateTime.Now }
             };
             return View(requestsList);
         }
--- Prezzieproject/Models/RequestsModel.cs
+++ Prezzieproject/Models/RequestsModel.cs
@@ -5,13 +5,13 @@ using System.Web;
 
 namespace Prezzieproject.Models
 {
-    public class RequestsModel
+    public class RequestsModel: SouvenirModel
     {
         public int souvenirID { get; set; }
         public string userName { get; set; }
         public int amount { get; set; }
         public double reward { get; set; }
         public string status { get; set; }
-
+        public DateTime requestDate { get; set; }
     }
 }
